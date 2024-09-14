70a98d2393d121b834f138b1497cf208e447b0f1
rocoriyan <rcorrigan.other+github@gmail.com>
2024-09-14 16:17:22 +0100

    filter out authors that existed over 200 years ago

--- script.js
+++ script.js
@@ -12,7 +12,8 @@ const getAllBooks = async () => {
 
 const processInfo = async () => {
     const bookData = await getAllBooks();
-    const items = bookData.results;
+    let items = bookData.results;
+    
     items.sort((a, b)=> a.id - b.id); //sorting by id
 
     items.forEach(item => { //capitalising subjects
@@ -21,6 +22,22 @@ const processInfo = async () => {
         });
         item.subjects = tempSubjects;
     });
+
+    items = items.filter((item) => { //remove entries whose authors have been confirmed to not exist in the past 200 years
+        currentYear = new Date().getFullYear();
+        for(let curr = 0; curr< (item.authors).length ; curr++){ //for every author in the authors object. if a single author is confirmed to have last existed over 200 years ago, the entire entry will be filtered out
+            //if we want to filter out authors with no confirmed birth or death dates, uncomment the below if statement
+            /*if(item.authors[curr].death_year == null && (item.authors[curr].birth_year == null || item.authors[curr].birth_year < currentYear-200)){
+                return false;
+            }*/ 
+            if(item.authors[curr].death_year != null){
+                if(item.authors[curr].death_year < currentYear-200){
+                    return false; //they died over 200 years before the current year, so they didn't exist in the past 200 years
+                }
+            }
+        }
+        return true;
+    })
 };
 
 processInfo();
